9f2b7c91084096aa1dabada224bf131087e9f697
junstat <junstat@163.com>
2022-01-06 16:27:24 +0800

    [LT] review 0029

---
+++ LeetCode/src/Q0099/Q0026RemoveDuplicatesFromSortedArray/Review.java
@@ -0,0 +1,61 @@
+package Q0099.Q0026RemoveDuplicatesFromSortedArray;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Given an integer array nums sorted in non-decreasing order,
+ * remove the duplicates in-place such that each unique element appears only once.
+ * The relative order of the elements should be kept the same.
+ * <p>
+ * Since it is impossible to change the length of the array in some languages,
+ * you must instead have the result be placed in the first part of the array nums.
+ * More formally, if there are k elements after removing the duplicates,
+ * then the first k elements of nums should hold the final result.
+ * It does not matter what you leave beyond the first k elements.
+ * <p>
+ * Return k after placing the final result in the first k slots of nums.
+ * <p>
+ * Do not allocate extra space for another array.
+ * You must do this by modifying the input array in-place with O(1) extra memory.
+ * <p>
+ * Example 1:
+ * Input: nums = [1,1,2]
+ * Output: 2, nums = [1,2,_]
+ * <p>
+ * Example 2:
+ * Input: nums = [0,0,1,1,1,2,2,3,3,4]
+ * Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+ */
+public class Review {
+
+    @Test
+    public void test() {
+        int[] nums = {1, 1, 2};
+        int[] expected = {1, 2};
+        int result = removeDuplicates(nums);
+        assertEquals(result, expected.length);
+        for (int i = 0; i < result; i++) {
+            assertEquals(nums[i], expected[i]);
+        }
+    }
+
+    @Test
+    public void test2() {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int[] expected = {0, 1, 2, 3, 4};
+        int result = removeDuplicates(nums);
+        assertEquals(result, expected.length);
+        for (int i = 0; i < result; i++) {
+            assertEquals(nums[i], expected[i]);
+        }
+    }
+
+
+    public int removeDuplicates(int[] nums) {
+        return 0;
+    }
+}
--- LeetCode/src/Q0099/Q0027RemoveElement/Solution.java
+++ LeetCode/src/Q0099/Q0027RemoveElement/Solution.java
@@ -1,14 +1,6 @@
 package Q0099.Q0027RemoveElement;
 
-import org.junit.Test;
-
 public class Solution {
-    @Test
-    public void test() {
-        int[] nums = {3, 2, 2, 3};
-        int val = 3;
-        System.out.println(removeElement(nums, val));
-    }
 
     public int removeElement(int[] nums, int val) {
         if (nums == null || nums.length == 0) return 0;
